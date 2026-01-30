922aa3bc9c1f0a9455c62f57b864448bb5d60011
renugopal-respo <renugopal21.p@gmail.com>
2026-01-30 16:12:27 +0530

    frontend folder cleanup

--- Routes/userRoute.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-import { createUser,loginUser } from '../controllers/Usercontoller.js';
-const router = express.Router();
-router.post('/createUser',createUser);
-router.post('/loginUser',loginUser);
-export default router;
