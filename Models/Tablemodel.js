2425f37a935a7cd4d2c530064e07d4b12d524202
renugopal-respo <renugopal21.p@gmail.com>
2026-02-07 14:13:59 +0530

    server files are removed

--- Models/Tablemodel.js
+++
@@ -1,8 +0,0 @@
-const db=require('../config/db');
-const getTables=async()=>{
-    const [rows]= await db.query("SHOW TABLES");
-    return rows;
-}
-module.exports={
-    getTables,
-}
--- Routes/propertyroutes.js
+++
@@ -1,6 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const PropertyController=require('../controllers/Propertycontroller')
-router.get('/getExclusiveProperties',PropertyController.getExclusiveProperties);
-router.get('/getInitialProperties',PropertyController.getInitialProperties);
-module.exports=router;
